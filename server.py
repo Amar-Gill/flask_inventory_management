5181ad82bdf72e76d45299bf0eded17622a81e29
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-22 22:06:05 +0800

    added create new store with db connection

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import peeweedbevolve
 from flask import Flask, render_template, request
-from models import db
+from models import db, Store
 app = Flask(__name__)
 
 @app.before_request
@@ -19,6 +19,17 @@ def migrate():
 @app.route("/")
 def index():
    return render_template('index.html')
+
+@app.route("/store", methods=["GET"])
+def store_get():
+   return render_template("store.html")
+
+@app.route("/store", methods=["POST"])
+def store_post():
+   print(request.form)
+   store = request.form['store']
+   Store.create(name = store)
+   return render_template("store.html", store=store)
 
 if __name__ == '__main__':
    app.run()
