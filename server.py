6739eadf971570787b7fb39306aa063244a762e8
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-23 15:11:20 +0800

    added new warehouse page

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import peeweedbevolve
 from flask import Flask, render_template, request, redirect, flash
-from models import db, Store
+from models import db, Store, Warehouse
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -22,11 +22,16 @@ def home():
    return render_template('index.html')
 
 @app.route("/store", methods=["GET"])
-def new():
+def new_store():
    return render_template("store.html")
 
+@app.route("/warehouse", methods=["GET"])
+def new_warehouse():
+   stores = Store.select()
+   return render_template("warehouse.html", stores=stores)
+
 @app.route("/store", methods=["POST"])
-def create():
+def create_store():
    print(request.form)
    store = Store(name = request.form['store'])
    if store.save():
@@ -34,6 +39,17 @@ def create():
       return redirect('/store')
    else:
       return render_template("store.html", name=request.form['store'])
+
+@app.route("/warehouse", methods=["POST"])
+def create_warehouse():
+   print(request.form)
+   store = Store.get_by_id(request.form['store_id'])
+   warehouse = Warehouse(location = request.form['location'], store=store)
+   if warehouse.save():
+      flash('Warehouse ' + str(warehouse.id) + ' was successfully saved!')
+      return redirect('/warehouse')
+   else:
+      return render_template("warehouse.html", location=request.form['location'])
 
 if __name__ == '__main__':
    app.run()
