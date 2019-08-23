461ac32b83558e57e42147135a32351bca0c52c3
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-23 20:08:25 +0800

    added data validation using peewee_validates library

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import os
 import peewee as pw
 import datetime
+from peewee_validates import ModelValidator
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
 db = PostgresqlExtDatabase(os.getenv('DATABASE'))
@@ -11,36 +12,51 @@ class BaseModel(pw.Model):
     updated_at = pw.DateTimeField(default=datetime.datetime.now)
 
     def save(self, *args, **kwargs):
-        self.errors = []
-        self.validate()
+        validator = type(self).CustomValidator(self)
+        validator.validate()
 
-        if len(self.errors) == 0:
+        self.errors = validator.errors
+
+        if self.errors:
+            return 0
+        else:
             self.updated_at = datetime.datetime.now()
             return super(BaseModel, self).save(*args, **kwargs)
-        else:
-            return 0
+
+    # def save(self, *args, **kwargs):
+    #     self.errors = []
+    #     self.validate()
+
+    #     if len(self.errors) == 0:
+    #         self.updated_at = datetime.datetime.now()
+    #         return super(BaseModel, self).save(*args, **kwargs)
+    #     else:
+    #         return 0
 
     class Meta:
         database = db
         legacy_table_names = False
 
+    class CustomValidator(ModelValidator):
+        pass
+
 class Store(BaseModel):
 
-    def validate(self):
-        duplicate_stores = Store.get_or_none(Store.name == self.name)
+    # def validate(self):
+    #     duplicate_stores = Store.get_or_none(Store.name == self.name)
 
-        if duplicate_stores:
-            self.errors.append('Store name not unique')
+    #     if duplicate_stores:
+    #         self.errors.append('Store name not unique')
 
     name = pw.CharField(unique=True)
 
 class Warehouse(BaseModel):
 
-    def validate(self):
-        duplicate_stores = Warehouse.get_or_none(Warehouse.store_id == self.store_id)
+    # def validate(self):
+    #     duplicate_stores = Warehouse.get_or_none(Warehouse.store_id == self.store_id)
 
-        if duplicate_stores:
-            self.errors.append('Selected store has a warehouse')
+    #     if duplicate_stores:
+    #         self.errors.append('Selected store has a warehouse')
 
     store = pw.ForeignKeyField(Store, backref='warehouse', unique=True)
     location = pw.TextField()
